9e9504c23a1e76c8dba12fc696874b7b4ede7d6c
JP Richardson <jprichardson@gmail.com>
2016-11-25 05:51:28 -0600

    test: add class / constructor

--- test.js
+++ test.js
@@ -18,3 +18,24 @@ test('should freze a regular object', t => {
 
   t.is(obj2.name, 'jp', 'name didnt change')
 })
+
+test('should freeze objects from constructor', t => {
+  t.plan(2)
+
+  class Person {
+    constructor (name) {
+      this.name = name
+    }
+  }
+
+  const FrozenPerson = proxyFreeze(Person)
+  const p = new FrozenPerson('jp')
+  p.name = 'bob'
+
+  process.once('warning', ({ name }) => {
+    t.is(name, 'ProxyFreezeWarning', 'proper warning type')
+    t.end()
+  })
+
+  t.is(p.name, 'jp', 'name didnt change')
+})
